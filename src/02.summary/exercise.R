96c988e1d5bd9dc9afdd912681f2f10a932b1514
a <a@a.com>
2021-01-14 15:03:04 +0900

    a

--- src/02.summary/exercise.R
+++ src/02.summary/exercise.R
@@ -34,3 +34,56 @@ text(55, 1.4, labels = paste0('평균 = ', round(mean(x), 3),
 
 hist(x, main='histogram', col = 7, prob = T)
 lines(density(x), lwd = 1.5, col = 2)
+abline(v = mean(x), lty = 2, lwd = 2, col = 4)
+# p1
+
+
+# 2.
+data(ex2_2)
+x = as.matrix(ex2_2)
+
+# 1)
+freq.table(x)
+#              Center Freq Cum-Fr Rel-Fr Rel-CFr
+# (24.2, 24.4]   24.3    7      7   0.07    0.07
+# (24.4, 24.6]   24.5   15     22   0.15    0.22
+# (24.6, 24.8]   24.7   16     38   0.16    0.38
+# (24.8, 25]     24.9   28     66   0.28    0.66
+# (25, 25.2]     25.1   21     87   0.21    0.87
+# (25.2, 25.4]   25.3    7     94   0.07    0.94
+# (25.4, 25.6]   25.5    6    100   0.06    1.00
+
+# 2)
+stem(x)
+# The decimal point is 1 digit(s) to the left of the |
+# 242 | 00000
+# 244 | 00000000000
+# 246 | 00000000000
+# 248 | 00000000000000000000000000
+# 250 | 0000000000000000000000000
+# 252 | 000000000000
+# 254 | 00000000
+# 256 | 00
+
+# 3)
+win.graph(7, 4)
+hist(x, prob = T, col = 7)
+lines(density(x), col = 2)
+abline(v = mean(x), lty = 2, lwd = 2, col = 4)
+mtext(round(mean(x), 3), side = 3, at = mean(x), col = 4, cex = 0.9)
+# p2-3
+
+# 4)
+mean(x); var(as.vector(x)); median(x)
+# [1] 24.917
+# [1] 0.1024354
+# [1] 24.9
+
+# 5)
+lsl = 25 - 0.5; usl = 25 + 0.5
+nd1 = sum(x < lsl); nd2 = sum(x > usl)
+pd1 = nd1 / length(x); pd2 = nd2 / length(x)
+cat('P(x < lsl) = ', pd1, '\tP(x > usl) = ', pd2, '\tP(d) = ', pd1 + pd2, '\n')
+# P(x < lsl) =  0.07 	P(x > usl) =  0.02 	P(d) =  0.09
+
+# 6)
